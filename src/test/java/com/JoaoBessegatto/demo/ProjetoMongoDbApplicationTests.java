64d32e056318bc4e3fa78dabb5047a6aa04db348
joao-pedro <bfjoaop@gmail.com>
2025-08-09 17:31:49 -0300

    Primeiro commit

---
+++ src/test/java/com/JoaoBessegatto/demo/ProjetoMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JoaoBessegatto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
